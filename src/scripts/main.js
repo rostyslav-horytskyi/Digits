a119b5ad6cc4a34ded61f4edf289fcaed79bf3ac
Rostyslav Horytskyi <gorzer.rostyslav@gmail.com>
2022-08-15 21:43:12 +0300

    create initial header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burgerButton = document.querySelector('.burger_button');
+const nav = document.querySelector('.navigation');
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('burger_button--open');
+  nav.classList.toggle('navigation--open');
+});
